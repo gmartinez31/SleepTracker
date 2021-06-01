749b028568986119bae109304f90c54b14f1f9d8
gmartinez31 <bhgustavo13@yahoo.com>
2021-05-31 23:19:21 -0600

    fix header

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -65,6 +65,14 @@ class SleepTrackerFragment : Fragment() {
 
         // add GridLayout
         val manager = GridLayoutManager(activity, 3)
+        manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int) = when (position) {
+                0 -> 3
+                else -> 1
+            }
+        }
+
+
         binding.sleepList.layoutManager = manager
 
         binding.sleepList.adapter = adapter
